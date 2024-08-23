5ff75868114b0c96bd7cd7e28c2549027736861b
gezever <gezever@skynet.be>
2024-08-23 16:56:36 +0200

    first commit

--- src/bash/model.sh
+++ src/bash/model.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+## sparql vind je hier https://dlcdn.apache.org/jena/binaries/apache-jena-5.1.0.tar.gz
   pushd ../result
   curl -LO 'https://datasets.omgeving.vlaanderen.be/be.vl.omg.dsi.distribution.dataset.dsi.publiek.ttl'
-  sparql --results=TTL --data='be.vl.omg.dsi.distribution.dataset.dsi.publiek.ttl'  --query '../sparql/model.rq'  > model.ttl
+  sparql --results=TTL --data='be.vl.omg.dsi.distribution.dataset.dsi.publiek.ttl'  --query '../sparql/model.rq' | sed -e 's/.*rdfs:label.*;//'  > model.ttl
   rdf2dot  model.ttl | dot -Tpng > model.png
   popd
-  #rdf2dot  ../result/model.ttl  > ../result/model.dot
+
